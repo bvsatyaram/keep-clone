93872d8186e2733a48225f23a008f96657571f29
BV Satyaram <bvsatyaram@gmail.com>
2017-09-02 22:18:06 +0530

    Add ability to toggle pinned status of a card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { card } from '../models/card';
 
 @Component({
   selector: 'app-card',
   template: `
     <div class="card">
       <div class="card-block">
+        <label class='custom-control custom-checkbox'>
+          <input type='checkbox' class='custom-control-input' [checked]='card.pinned' (change)='togglePin(card)' />
+          <span class='custom-control-indicator'></span>
+        </label>
         <p class="card-text">{{card.text}}</p>
       </div>
     </div>
@@ -20,6 +25,10 @@ export class CardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  togglePin(card: card) {
+    card.pinned = !card.pinned;
   }
 
 }
