56b716fa7fb52c4ebe9595da798361f2e9b96e5e
BV Satyaram <bvsatyaram@gmail.com>
2017-09-02 21:23:52 +0530

    Add card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  template: `
+    <div class="card">
+      <div class="card-block">
+        <p class="card-text">Card Text</p>
+      </div>
+    </div>
+  `,
+  styles: [],
+  host: {'class': 'col-2'}
+})
+export class CardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
